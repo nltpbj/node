df1e2e88c42dc4ab57089b308687fd0de44541b0
nltpbj <nltpbj1725@naver.com>
2024-05-30 18:34:06 +0900

    bookhome

--- shop/routes/books.js
+++ shop/routes/books.js
@@ -1,48 +1,78 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db');
+var multer = require('multer');
 
-/* 도서등록 */
-router.post('/insert', function(req, res, next) {
-    const title=req.body.title;
-    const price=req.body.price;
-    const contents=req.body.contents;
-    const isbn=req.body.isbn;
-    const publisher=req.body.publisher;
-    const author=req.body.authors;
-    const image=req.body.thumbnail;
-    //console.log(title,price,contents,isbn,publisher,author,image);
-
-    const sql ='select * from books where isbn=?';
-    db.get().query(sql, [isbn], function(err, rows){
-        if(rows.length==0){
-           const sql1='insert into books(title,price,contents,isbn,publisher,author,image) values(?,?,?,?,?,?,?)';
-           db.get().query(sql1, [title,price,contents,isbn,publisher,author,image], function(err, rows){
-            if(err){
-                //console.log('err.............', err);
-                res.send({result:0})
-            }else{
-                res.send({result:1});
-            }
-           });
-        }else{
-            res.send({result:0});
-        }
-    });
+var upload = multer({
+  storage:multer.diskStorage({
+    destination:(req, file, done)=>{
+      done(null, './public/books')
+    },
+    filename:(req, file, done)=>{
+      var filename=Date.now() + '.jpg';
+      done(null, filename);
+    }
+  })
 });
 
-//도서목록 query get으로 받을때 테스트:/books/list?page=1&size=5
+//이미지업로드
+router.post('/upload',upload.single('file'), function(req, res){
+  const bid=req.body.bid;
+  const filename="/books/" + req.file.filename;
+  const sql="update books set bigimage=?, updateDate=now() where bid=?";
+  db.get().query(sql, [filename, bid], function(err, rows){
+    if(err){
+      console.log('err...........', err);
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
+/*도서등록*/
+router.post('/insert', function(req, res, next) {
+  const title=req.body.title;
+  const price=req.body.price;
+  const contents=req.body.contents;
+  const isbn=req.body.isbn;
+  const publisher=req.body.publisher;
+  const author=req.body.authors;
+  const image=req.body.thumbnail;
+  //console.log(title,price,contents,isbn,publisher,author,image);
+  console.log('.............', author);
+  const sql='select * from books where isbn=?';
+  db.get().query(sql, [isbn], function(err, rows){
+    if(rows.length==0){
+      const sql1='insert into books(title,price,contents,isbn,publisher,author,image) values(?,?,?,?,?,?,?)';
+      db.get().query(sql1, [title,price,contents,isbn,publisher,author,image], function(err, rows){
+        res.send({result:1});
+      })
+    }else{
+      res.send({result:0});
+    }
+  });  
+});
+
+//도서목록 테스트:/books/list?page=1&size=5
 router.get('/list', function(req, res){
     const page=parseInt(req.query.page);
     const size=parseInt(req.query.size);
-    let sql ="select *,date_format(regdate,'%Y-%m-%d') fmtdate,format(price,0) fmtprice";
+    const key=req.query.key;
+    const word=req.query.word;
+    const uid=req.query.uid;
+    let sql ="select *,date_format(regdate,'%Y-%m-%d %T') fmtdate,format(price,0) fmtprice,";
+        sql+="(select count(*) from likes where books.bid=likes.bid) lcnt,";
+        sql+="(select count(*) from likes where books.bid=likes.bid and uid=?) ucnt"
         sql+=" from books ";
+        sql+=` where ${key} like '%${word}%'`;
         sql+=" order by bid desc";
         sql+=" limit ?,?";
   
-    db.get().query(sql, [(page-1)*size, size], function(err, rows){
+    db.get().query(sql, [uid,(page-1)*size, size], function(err, rows){
       const documents = rows;
       sql = "select count(*) total from books";
+      sql+=` where ${key} like '%${word}%'`;
       db.get().query(sql, function(err, rows){
         const count=rows[0].total;
         res.send({count, documents});
@@ -50,18 +80,58 @@ router.get('/list', function(req, res){
     });
   });
 
-  //도서삭제
-  router.post('/delete', function(req, res){
+//도서삭제
+router.post('/delete', function(req, res){
+  const bid=req.body.bid;
+  const sql="delete from books where bid=?";
+  db.get().query(sql, [bid], function(err, rows){
+    if(err){
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
+//도서정보 Read
+router.get('/read/:bid', function(req, res){
+  const bid=req.params.bid;
+  const sql="select *,date_format(updatedate,'%Y-%m-%d %T') fmtdate from books where bid=?";
+  db.get().query(sql, [bid], function(err, rows){
+    res.send(rows[0]);
+  });
+});
+
+//도서정보 수정
+router.post('/update', function(req, res){
+  const bid=req.body.bid;
+  const title=req.body.title;
+  const price=req.body.price;
+  const author=req.body.author;
+  const contents=req.body.contents;
+  const sql="update books set title=?,price=?,author=?,contents=?,updatedate=now() where bid=?";
+  db.get().query(sql, [title,price,author,contents,bid], function(err, rows){
+    if(err) {
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    }
+  });
+});
+
+//좋아요추가
+router.post('/likes/insert', function(req, res){
+    const uid=req.body.uid;
     const bid=req.body.bid;
-    const sql="delete from books where bid=?";
-    db.get().query(sql, [bid], function(err, rows){
+    const sql="insert into likes(uid,bid) values(?,?)"
+    db.get().query(sql, [uid, bid], function(err, rows){
         if(err){
+            console.log('err......', err);
             res.send({result:0});
         }else{
             res.send({result:1});
         }
     });
-  });
+});
 
-  
 module.exports = router;
