3743739ce92fc08a9e5292d04ba5e2417e25a49d
nltpbj <nltpbj1725@naver.com>
2024-06-04 18:26:19 +0900

    purchase

--- shop/routes/orders.js
+++ shop/routes/orders.js
@@ -42,4 +42,70 @@ router.post('/insert', function(req,res){
     }
   })
 });
+
+//사용자별 주문목록
+router.get('/list', function(req, res){
+  const uid=req.query.uid;
+  let sql="select *, date_format(pdate,'%Y-%m-%d %T') as fmtdate,";
+  sql+=" format(sum,0) fmtsum";
+  sql+=" from purchase where uid=?";
+  sql+=" order by pdate desc";
+  db.get().query(sql, [uid], function(err, rows){
+    res.send(rows);
+  });
+});
+
+//특정주문의 주문상품목록
+router.get('/books', function(req, res){
+  const pid=req.query.pid;
+  let sql="select o.*, b.title, b.image, format(o.price,0) fmtprice, format(o.price*o.qnt,0) fmtsum";
+  sql+=" from orders o, books b";
+  sql+=" where o.bid=b.bid and pid=?";
+  db.get().query(sql, [pid], function(err, rows){
+    res.send(rows);
+  });
+});
+
+//관리자 주문목록
+router.get('/admin/list', function(req, res){
+  const key=req.query.key;
+  const word=req.query.word;
+  const page=parseInt(req.query.page);
+  const size=parseInt(req.query.size);
+  let sql="select *, ";
+  sql+=" date_format(pdate,'%Y-%m-%d %T') as fmtdate,";
+  sql+=" format(sum,0) fmtsum";
+  sql+=" from purchase";
+  sql+=` where ${key} like ?`;
+  sql+=" order by pdate desc";
+  sql+=" limit ?, ?;"
+  db.get().query(sql, [`%${word}%`, (page-1)*size, size], function(err, rows){
+    let documents = rows;
+    sql="select count(*) count from purchase";
+    sql+=` where ${key} like ?`;
+    db.get().query(sql, [`%${word}%`], function(err, rows){
+      const count=rows[0].count;
+      if(count==0){
+        res.send({documents:[], count});
+      }else{
+        res.send({documents, count});
+      }
+    });
+  });
+});
+
+//주문상태변경
+router.post('/status', function(req, res){
+  const pid=req.body.pid;
+  const status=req.body.status;
+  const sql="update purchase set status=? where pid=?";
+  db.get().query(sql, [status, pid], function(err, rows){
+    if(err){
+      res.send({result:0});
+    }else{
+      res.send({result:1});
+    } 
+  })
+});
 module.exports = router;
+
